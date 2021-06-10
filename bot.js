1a9b12df735d8a5d1ab870729cfdc584acd8aba8
Danos <garciadanos@gmail.com>
2021-06-10 01:58:53 -0300

    MusicLogBot-v1

---
+++ bot.js
@@ -0,0 +1,62 @@
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+const client = new Discord.Client();
+
+//TODO:Agregar comandos para ver la playlist, quitar canciones
+//Array de prefijos para pedir una cancion
+let songCall = ["_p", "_P", "!p", "!P", "/p", "/P"];
+let songList = [];
+//Puesta en marcha del bot
+client.on("ready", () => {
+  console.log("Bot Ensamblado");
+  client.user.setActivity("Saving your activity");
+});
+
+//Saludo
+//FIXME: solo saluda cuando alguien manda un mensaje
+if (client.presence.status == "online") {
+  client.once("message", (message) => {
+    message.channel.send(
+      `Music Bot Ready to Save Song's Set :fist: :sweat_drops:`
+    );
+  });
+}
+
+//Comand example
+client.on("message", (message) => {
+  if (message.content === `${prefix}ping`) {
+    message.channel.send("Pong.");
+  } else if (message.content === `${prefix}beep`) {
+    message.channel.send("Boop.");
+  } else if (message.content === `${prefix}server`) {
+    message.channel.send(
+      `Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`
+    );
+  }
+});
+//Checkea que el mensaje empiece con la llamada a la cancion
+for (let i of songCall) {
+  client.on("message", (message) => {
+    if (!message.content.startsWith(`${i}`)) {
+      return;
+    } else if (message.content.startsWith(`${i}`)) {
+      //Trimea la parte inicial de la solicitud de cancion y espacios de principio y fin
+      const songName = message.content.slice(i.length).trim();
+      message.channel.send(`Se agrego: ${songName} a la Playlist`);
+      songList.push(songName);
+    }
+  });
+}
+//Muestra Lista de Canciones en la Playlist
+client.on("message", (message) => {
+  if (!message.content === `${prefix}songs`) {
+    return;
+  } else if (message.content === `${prefix}songs`) {
+    message.channel.send(">>> `Playlist`");
+    for (let u of songList) {
+      message.channel.send("`" + `${u}\n` + "`");
+    }
+  }
+});
+//Coneccion a sv
+client.login(token);
